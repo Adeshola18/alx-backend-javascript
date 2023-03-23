ca86af8f23d3d7b0000d070300be1a51be736403
Timothy Atobatele <oladimeji.atobatele@gmail.com>
2023-03-23 23:29:17 +0100

    deleting

--- 0x05-Node_JS_basic/0-console.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-/* create a function named displayMessage that prints in STDOUT the string argument. */
-
-const displayMessage = (msg) => {
-  console.log(msg);
-};
-
-module.exports = displayMessage;
