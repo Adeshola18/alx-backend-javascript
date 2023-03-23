e2ed0cb71ed25661b7676180b30621e9749241b7
Timothy Atobatele <oladimeji.atobatele@gmail.com>
2023-03-23 16:55:40 +0100

    all work redone

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,16 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((values) => {
+      const resArray = [];
+      for (let i = 0; i < values.length; i += 1) {
+        resArray.push({
+          status: values[i].status,
+          value: values[i].value ? values[i].value : `${values[i].reason}`,
+        });
+      }
+      return resArray;
+    });
+}
